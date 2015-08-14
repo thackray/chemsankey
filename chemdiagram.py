63fcfb1771a4f968705e91be9043d321a476596e
thackray <thackray@mit.edu>
2015-08-13 23:17:11 -0400

    Added too many features to name

--- chemdiagram.py
+++ chemdiagram.py
@@ -4,15 +4,15 @@ from matplotlib.path import Path
 import numpy as np
 
 class ChemSankey(object):
-    def __init__(self, ):
+    def __init__(self,):
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(1,1,1,xticks=[],yticks=[],
                                        ylim=(-1.,1.), xlim=(-1.,1.)
                                        )
 
-    def _add_node(self,loc,color='cyan',label='',
+    def _draw_node(self,loc,color='cyan',label='',size=0.1,
                   **kwargs):
-        ell = Ellipse(loc, 0.1, 0.075,
+        ell = Ellipse(loc, size, size*0.75,
                       facecolor=color,
                       alpha=0.9,
                       edgecolor='k',
@@ -38,21 +38,126 @@ class ChemSankey(object):
                        **kwargs)
         self.ax.add_artist(PP)
         
+    def _set_node_positions(self, positions=None):
+        if positions == None:
+            node_positions = {}
+            for i,node in enumerate(self.node_labels):
+                xy = [0.5,0.9-2*i/float(len(self.node_labels))]
+                if i%2:
+                    xy[0] *= -1
+                node_positions[node] = xy
+        else:
+            assert type(positions) == dict, 'given node positions must be dict'
+            for node in self.node_labels:
+                assert node in positions.keys(), '%s not in position dict'%node
+        self.node_positions = node_positions
+        return
 
-locs = {'A':np.array([0.,.7]),
-        'B':np.array([.7,0.]),
-        'C':np.array([.7,.7])
-        }
+    def _make_connections(self, ):
+        bends = [2., 1.5, 0.5]
+        conn_counter = {}
+        for i,conn in enumerate(self.connections):
+            FROM = self.connections[conn]['from']
+            TO = self.connections[conn]['to']
+            conn_name = '_'.join(sorted([FROM, TO]))
+            if not (conn_name in conn_counter):
+                conn_counter[conn_name] = 0
+            self._make_connection(self.node_positions[FROM],
+                                  self.node_positions[TO],
+                                  bend=bends[conn_counter[conn_name]],
+                                  width=self.line_widths[i],
+                                  color=self.colors[self.connections \
+                                                        [conn]['via']],
+                                  )
+
+            conn_counter[conn_name] += 1
+
+    def _draw_node_label(self, loc, label):
+        self.ax.text(loc[0],loc[1],label)
+        return
+
+    def _draw_nodes(self, ):
+        for i,node in enumerate(self.node_labels):
+            self._draw_node(self.node_positions[node],
+                            size=self.node_sizes[i])
+            self._draw_node_label(self.node_positions[node],
+                                  node,
+                                  )
+        return
+
+    def _setup_colors(self, connections, colors):
+        diff_conn = []
+        for conn in connections:
+            if not (connections[conn]['via'] in diff_conn):
+                diff_conn.append(connections[conn]['via'])
+        if colors == None:
+            colors_dict = {}
+            for dc in diff_conn:
+                colors_dict[dc] = 'k'
+        elif type(colors) == list:
+            assert len(colors) == len(diff_conn), ('must give one color for',
+                                                   'each',
+                                                   'different connection type')
+            colors_dict = {}
+            for i,dc in enumerate(colors):
+                colors_dict[dc]=colors[i]
+        elif type(colors) == dict:
+            for dc in diff_conn:
+                assert dc in colors.keys(), 'connection %s not in colors'%dc
+            colors_dict = colors
+        else:
+            raise IOError, 'unknown type for connection colors definition'
+        self.colors = colors_dict
+        return
+
+    def setup_chem(self, species, reactions, fluxes, concs,
+                   ):
+        self.node_labels = species
+        self.line_widths = fluxes
+        self.node_sizes = 2*concs/max(concs)/len(concs)
+        connections = {}
+        for i,key in enumerate(reactions):
+            connections[i] = {'from':reactions[key]['R'][0],
+                              'to':reactions[key]['P'][0],
+                              'via':reactions[key]['R'][1],
+                              }
+        self.connections = connections
+        return
+
+    def setup_art(self, colors=None, positions=None):
+        self._setup_colors(self.connections,colors)
+        self._set_node_positions(positions=positions)
+        self._make_connections()
+        self._draw_nodes()
+        return
 
 
-
+species = ['A','B','C','D','E']
+conspecies = ['OH','NO']
+reactions = {0:{'R':['A','OH'],'P':['B']},
+             1:{'R':['B','NO'],'P':['C']},
+             2:{'R':['C','OH'],'P':['D']},
+             3:{'R':['C','NO'],'P':['D']},
+             4:{'R':['C','NO'],'P':['E']},
+             5:{'R':['A','NO'],'P':['C']},
+             }
+fluxes = np.array([8.,8.,5.,4.,1.,2.])
+concs = np.array([2.,5.,3.,1.1,2.,1.5])
 
 CS = ChemSankey()
-CS._make_connection(locs['A'],locs['B'])
-CS._make_connection(locs['A'],locs['C'],width=4)
-CS._add_node(locs['A'])
-CS._add_node(locs['B'])
-CS._add_node(locs['C'])
+CS.setup_chem(species,reactions,fluxes,concs)
+CS.setup_art(colors={'OH':'r','NO':'b'})
+
+#locs = {'A':np.array([0.,.7]),
+#        'B':np.array([.7,0.]),
+#        'C':np.array([.7,.7])
+#        }
+
+#CS._make_connection(locs['A'],locs['B'])
+#CS._make_connection(locs['A'],locs['C'],width=4)
+#CS._add_node(locs['A'])
+#CS._add_node(locs['B'])
+#CS._add_node(locs['C'])
 #fig = plt.figure()
 #ax = fig.add_subplot(1,1,1,xticks=[],yticks=[],title='',
 #                     ylim=(-2,2),xlim=(-2,2))
